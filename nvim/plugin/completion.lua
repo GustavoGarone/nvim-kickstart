92b6d13cc83607bce6049c5086de3bac61a0d367
Minze <gustavogarone@protonmail.com>
2024-08-27 09:08:09 -0300

    completion rollback

--- nvim/plugin/completion.lua
+++ nvim/plugin/completion.lua
@@ -1,157 +1,163 @@
-return {
-  {
-    'windwp/nvim-autopairs',
-    config = function()
-      require('nvim-autopairs').setup {}
-      require('nvim-autopairs').remove_rule '`'
-    end,
-  },
+if vim.g.did_load_completion_plugin then
+  return
+end
+vim.g.did_load_completion_plugin = true
 
-  { -- completion
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
-    dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-nvim-lsp-signature-help',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-calc',
-      'hrsh7th/cmp-emoji',
-      'saadparwaiz1/cmp_luasnip',
-      'f3fora/cmp-spell',
-      'ray-x/cmp-treesitter',
-      'kdheepak/cmp-latex-symbols',
-      'jmbuhr/cmp-pandoc-references',
-      'L3MON4D3/LuaSnip',
-      'rafamadriz/friendly-snippets',
-      'onsails/lspkind-nvim',
-      'jmbuhr/otter.nvim',
+local cmp = require('cmp')
+local lspkind = require('lspkind')
+local luasnip = require('luasnip')
+
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
+
+local function has_words_before()
+  local unpack_ = unpack or table.unpack
+  local line, col = unpack_(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+end
+
+---@param source string|table
+local function complete_with_source(source)
+  if type(source) == 'string' then
+    cmp.complete { config = { sources = { { name = source } } } }
+  elseif type(source) == 'table' then
+    cmp.complete { config = { sources = { source } } }
+  end
+end
+
+cmp.setup {
+  completion = {
+    completeopt = 'menu,menuone,noinsert',
+    -- autocomplete = false,
+  },
+  formatting = {
+    format = lspkind.cmp_format {
+      mode = 'symbol_text',
+      with_text = true,
+      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+
+      menu = {
+        buffer = '[BUF]',
+        nvim_lsp = '[LSP]',
+        nvim_lsp_signature_help = '[LSP]',
+        nvim_lsp_document_symbol = '[LSP]',
+        nvim_lua = '[API]',
+        path = '[PATH]',
+        luasnip = '[SNIP]',
+      },
     },
-    config = function()
-      local cmp = require 'cmp'
-      local luasnip = require 'luasnip'
-      local lspkind = require 'lspkind'
-
-      local has_words_before = function()
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
-      end
-
-      cmp.setup {
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        completion = { completeopt = 'menu,menuone,noinsert' },
-        mapping = {
-          ['<C-f>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-d>'] = cmp.mapping.scroll_docs(4),
-
-          ['<C-n>'] = cmp.mapping(function(fallback)
-            if luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<C-p>'] = cmp.mapping(function(fallback)
-            if luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<c-y>'] = cmp.mapping.confirm {
-            select = true,
-          },
-          ['<CR>'] = cmp.mapping.confirm {
-            select = true,
-          },
-
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif has_words_before() then
-              cmp.complete()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-
-          ['<C-l>'] = cmp.mapping(function()
-            if luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            end
-          end, { 'i', 's' }),
-          ['<C-h>'] = cmp.mapping(function()
-            if luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            end
-          end, { 'i', 's' }),
-        },
-        autocomplete = false,
-
-        ---@diagnostic disable-next-line: missing-fields
-        formatting = {
-          format = lspkind.cmp_format {
-            mode = 'symbol',
-            menu = {
-              otter = '[🦦]',
-              nvim_lsp = '[LSP]',
-              nvim_lsp_signature_help = '[sig]',
-              luasnip = '[snip]',
-              buffer = '[buf]',
-              path = '[path]',
-              spell = '[spell]',
-              pandoc_references = '[ref]',
-              tags = '[tag]',
-              treesitter = '[TS]',
-              calc = '[calc]',
-              latex_symbols = '[tex]',
-              emoji = '[emoji]',
-            },
-          },
-        },
-        sources = {
-          -- { name = 'otter' }, -- for code chunks in quarto
-          { name = 'path' },
-          { name = 'nvim_lsp_signature_help' },
-          { name = 'nvim_lsp' },
-          { name = 'luasnip', keyword_length = 3, max_item_count = 3 },
-          { name = 'pandoc_references' },
-          { name = 'buffer', keyword_length = 5, max_item_count = 3 },
-          { name = 'spell' },
-          { name = 'treesitter', keyword_length = 5, max_item_count = 3 },
-          { name = 'calc' },
-          { name = 'latex_symbols' },
-          { name = 'emoji' },
-        },
-        view = {
-          entries = 'native',
-        },
-        window = {
-          documentation = {
-            border = require('misc.style').border,
-          },
-        },
-      }
-
-      -- for friendly snippets
-      require('luasnip.loaders.from_vscode').lazy_load()
-      -- for custom snippets
-      require('luasnip.loaders.from_vscode').lazy_load { paths = { vim.fn.stdpath 'config' .. '/snips' } }
-      -- link quarto and rmarkdown to markdown snippets
-      luasnip.filetype_extend('quarto', { 'markdown' })
-      luasnip.filetype_extend('rmarkdown', { 'markdown' })
+  },
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
     end,
   },
-
+  mapping = {
+    ['<C-b>'] = cmp.mapping(function(_)
+      if cmp.visible() then
+        cmp.scroll_docs(-4)
+      else
+        complete_with_source('buffer')
+      end
+    end, { 'i', 'c', 's' }),
+    ['<C-f>'] = cmp.mapping(function(_)
+      if cmp.visible() then
+        cmp.scroll_docs(4)
+      else
+        complete_with_source('path')
+      end
+    end, { 'i', 'c', 's' }),
+    ['<C-n>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      -- expand_or_jumpable(): Jump outside the snippet region
+      -- expand_or_locally_jumpable(): Only jump inside the snippet region
+      elseif luasnip.expand_or_locally_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { 'i', 'c', 's' }),
+    ['<C-p>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 'c', 's' }),
+    -- toggle completion
+    ['<C-e>'] = cmp.mapping(function(_)
+      if cmp.visible() then
+        cmp.close()
+      else
+        cmp.complete()
+      end
+    end, { 'i', 'c', 's' }),
+    ['<C-y>'] = cmp.mapping.confirm {
+      select = true,
+    },
+  },
+  sources = cmp.config.sources {
+    -- The insertion order influences the priority of the sources
+    { name = 'nvim_lsp', keyword_length = 3 },
+    { name = 'nvim_lsp_signature_help', keyword_length = 3 },
+    { name = 'buffer' },
+    { name = 'path' },
+  },
+  enabled = function()
+    return vim.bo[0].buftype ~= 'prompt'
+  end,
+  experimental = {
+    native_menu = false,
+    ghost_text = true,
+  },
 }
+
+cmp.setup.filetype('lua', {
+  sources = cmp.config.sources {
+    { name = 'nvim_lua' },
+    { name = 'nvim_lsp', keyword_length = 3 },
+    { name = 'path' },
+  },
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'nvim_lsp_document_symbol', keyword_length = 3 },
+    { name = 'buffer' },
+    { name = 'cmdline_history' },
+  },
+  view = {
+    entries = { name = 'wildmenu', separator = '|' },
+  },
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources {
+    { name = 'cmdline' },
+    { name = 'cmdline_history' },
+    { name = 'path' },
+  },
+})
+
+vim.keymap.set({ 'i', 'c', 's' }, '<C-n>', cmp.complete, { noremap = false, desc = '[cmp] complete' })
+vim.keymap.set({ 'i', 'c', 's' }, '<C-f>', function()
+  complete_with_source('path')
+end, { noremap = false, desc = '[cmp] path' })
+vim.keymap.set({ 'i', 'c', 's' }, '<C-o>', function()
+  complete_with_source('nvim_lsp')
+end, { noremap = false, desc = '[cmp] lsp' })
+vim.keymap.set({ 'c' }, '<C-h>', function()
+  complete_with_source('cmdline_history')
+end, { noremap = false, desc = '[cmp] cmdline history' })
+vim.keymap.set({ 'c' }, '<C-c>', function()
+  complete_with_source('cmdline')
+end, { noremap = false, desc = '[cmp] cmdline' })
